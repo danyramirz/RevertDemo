5a0ee831655f1a5e052c23387d5278455fed3c87
Daniel Ramirez <Daniel Ramirez@DANYBOY>
2016-10-26 10:06:00 -0700

    commit 2

--- ReertDemoSolution/ReertDemo/Program.cs
+++ ReertDemoSolution/ReertDemo/Program.cs
@@ -11,6 +11,7 @@ namespace ReertDemo
         static void Main(string[] args)
         {
             Console.WriteLine("First change");
+            Console.WriteLine("second change");
         }
     }
 }
